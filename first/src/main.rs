6691e85440f4ea780987763ba153a9739cc353ee
97gushan <97gustavh@gmail.com>
2019-09-04 13:53:55 +0200

    Experimental parse (#1)
    * create tree with priorities on operators
    
    * change operation representation to enum
    
    * parse and remove whitespace when creating a leef
    
    * remove unnecessary typecast to char
    
    * fixed spelling
    
    * move parser to module
    
    * changed name to Node
    
    * automatic formating

--- first/src/main.rs
+++ first/src/main.rs
@@ -1,84 +1,15 @@
-extern crate nom;
+mod parser;
 
-use nom::{
-  IResult,
-  bytes::complete::{tag, take_while_m_n, is_a},
-  combinator::map_res,
-  sequence::tuple,
-  character::complete::{digit1, char},
-  error::ErrorKind};
-
-
-fn from_string(input: &str) -> Result<i32, std::num::ParseIntError> {
-  i32::from_str_radix(input, 10)
-}
-
-
-#[derive(Debug)]
-enum Tree {
-    Cons(char, Box<Tree>, Box<Tree>),
-    Leef(i32),
-    Nil,
-}
-
-use crate::Tree::{Cons, Nil, Leef};
-
-
-fn create_tree(input: &str) -> Tree{
-
-    let test = parse_digit(input);
-
-    let left_tree: Tree;
-    let right_tree: Tree;
-    let operator: &str;
-    let root;
-
-    match test{
-        Ok(i) =>{
-            let b = i;
-            
-            let input = b.0;
-            left_tree = Leef(from_string(b.1).unwrap());
-
-            if input.len() > 0 {
-                let a = parse_operator(input).unwrap();
-
-                right_tree = create_tree(&a.0);
-                operator = a.1;
-
-                root = Cons(operator.parse().unwrap(), Box::new(left_tree), Box::new(right_tree));
-
-            }else{
-                root = left_tree;
-            }
-        },
-        Err(error) =>{
-            println!("{:?}", error);
-            left_tree = Leef(101);
-            root = left_tree;
-        }
-    }    
-
-    return root;
-}
-
-fn parse_digit(input: &str) -> IResult<&str, &str>{
-    digit1(input)
-}
-
-fn parse_operator(input: &str) -> IResult<&str, &str>{
-    let (a, b) = tag("+")(input)?;
-
-    Ok((a, b))
-}
+pub use crate::parser::parse_expression;
 
 
 fn main() {
 
-    let input = "101+1+2+3";
+    //let input = "101*2*3+2/44+1*2+3* 10-23*5/2 + 2 + 1 *3 - 2 /5 *2 + 1";
+    let input = "1 + 2 * 3 - 5";
 
-    
-    let tree = create_tree(input);
+
+    let tree = parse_expression::create_tree(input);
 
     println!("{:#?}", tree);
 }
---
+++ first/src/parser.rs
@@ -0,0 +1,89 @@
+extern crate nom;
+
+pub mod parse_expression {
+
+    use nom::{
+        character::complete::{digit1, space0},
+        FindSubstring, IResult,
+    };
+
+    #[derive(Debug)]
+    pub enum Tree {
+        Node(Op, Box<Tree>, Box<Tree>),
+        Leaf(i32),
+    }
+
+    #[derive(Debug)]
+    pub enum Op {
+        Add,
+        Sub,
+        Mult,
+        Div,
+    }
+
+    /// Creates a new node in the tree
+    ///
+    /// # Arguments
+    ///
+    /// * `input` - A substring that will be split into a node and subtrees
+    /// * `i` - Index of the operator that will act as node
+    fn create_node(input: &str, i: usize) -> Tree {
+        let operator_char: &str = &input[i..=i];
+
+        let operator: Op;
+
+        if operator_char == "+" {
+            operator = Op::Add;
+        } else if operator_char == "-" {
+            operator = Op::Sub;
+        } else if operator_char == "*" {
+            operator = Op::Mult;
+        } else {
+            operator = Op::Div;
+        }
+        let left_tree: Tree = create_tree(&input[..i]);
+        let right_tree: Tree = create_tree(&input[i + 1..]);
+
+        Tree::Node(operator, Box::new(left_tree), Box::new(right_tree))
+    }
+
+    fn create_leaf(input: &str) -> Tree {
+        let removed_whitespace: IResult<&str, &str> = space0(input);
+
+        let trimmed_input;
+
+        match removed_whitespace {
+            Ok(res) => trimmed_input = res.0,
+            Err(_error) => panic!("Panic when removing whitespace"),
+        }
+        let parsed: IResult<&str, &str> = digit1(trimmed_input);
+
+        match parsed {
+            Ok(result) => Tree::Leaf(result.1.parse().unwrap()),
+            Err(_error) => panic!("This is not a number!!!!"),
+        }
+    }
+
+    pub fn create_tree(input: &str) -> Tree {
+        let node: Tree;
+
+        // find operators with +- first so the come higher in the tree
+        // this so when the tree is evaluated the lowest nodes are done first
+        // this means that if all the */ gets evaluated before +-
+        if let Some(i) = input.find_substring("+") {
+            node = create_node(input, i);
+        } else if let Some(i) = input.find_substring("-") {
+            node = create_node(input, i);
+        } else if let Some(i) = input.find_substring("*") {
+            node = create_node(input, i);
+        } else if let Some(i) = input.find_substring("/") {
+            node = create_node(input, i);
+        } else if let Some(i) = input.find_substring("%") {
+            node = create_node(input, i);
+        } else {
+            node = create_leaf(input);
+        }
+
+        node
+    }
+}
