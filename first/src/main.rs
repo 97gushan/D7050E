306e536fe3c48c9275b163954c9e1d554aa48f2e
97gushan <97gustavh@gmail.com>
2019-09-11 10:29:51 +0200

    added tests

--- first/src/main.rs
+++ first/src/main.rs
@@ -1,6 +1,8 @@
 
 pub mod ast;
 
+
+
 #[macro_use] extern crate lalrpop_util;
 
 lalrpop_mod!(pub parser);
@@ -15,40 +17,196 @@ fn main() {
     //              a = 3 + 3 - 2*5;
     //              let b = 10;";
 
-    let input = "if(1==2){
+    // let input = "if(true){
+    //                 let a: i32 = 2;
+    //             }else{
+    //                 let k: i32 = 10;
+    //             };
+                
+    //             let a: i32 = 10;
+    //             let b: bool = 2==2 || 1 > 5;
+    //             let c: i32 = 123;";
+
+    //let input = "let a: bool = 1 == 2 || true && 5 < 10;";
+
+    let input = "if(true){ 
                     let a: i32 = 2;
-                };
-                
-                let a: i32 = 10;
-                let b: bool = 2==2;";
-                
+                }else if(false){
+                    let c: i32 = 2;
+                }else{
+                    let b: i32 = 3;
+                };";       
 
     //let expr = parser::ExprParser::new().parse(input).unwrap();
 
     let set = parser::SeparateLinesParser::new().parse(input).unwrap();
 
-    println!("{:#?}", set);
-
-  
+    println!("{:#?}", set);  
 }
 
-#[test]
-fn testExpr(){
 
 
-    assert_eq!(parser::ExprParser::new().parse("1+2").unwrap(), Box::new(
-                                                                    crate::ast::ExprTree::BinNode(
-                                                                        Box::new(crate::ast::ExprTree::Number(1)), 
-                                                                        crate::ast::BinOp::Add,
-                                                                        Box::new(crate::ast::ExprTree::Number(2)))));
+
+
+
+#[cfg(test)]
+mod test{
+
+    use super::*;
+
+    use crate::ast::*;
+
+    #[test]
+    fn test_expr(){
+
+
+        assert_eq!(parser::ExprParser::new().parse("1+2").unwrap(), 
+                Box::new(ExprTree::BinNode(
+                            Box::new(ExprTree::Number(1)), 
+                            BinOp::Add,
+                            Box::new(ExprTree::Number(2)))));
+        
+        assert_eq!(parser::ExprParser::new().parse("1 * (2 - 2)").unwrap(), 
+                Box::new( ExprTree::BinNode(
+                            Box::new(ExprTree::Number(1)),                                                                                 
+                            BinOp::Mul,
+                            Box::new(ExprTree::BinNode(
+                                Box::new(ExprTree::Number(2)),
+                                BinOp::Sub,
+                                Box::new(ExprTree::Number(2)),
+                            )))));
+    }
     
-    assert_eq!(parser::ExprParser::new().parse("1 * 2 - 2").unwrap(), Box::new(
-                                                                        crate::ast::ExprTree::BinNode(
-                                                                            Box::new(crate::ast::ExprTree::BinNode(
-                                                                                Box::new(crate::ast::ExprTree::Number(1)),
-                                                                                crate::ast::BinOp::Mul,
-                                                                                Box::new(crate::ast::ExprTree::Number(2)),
-                                                                            )), 
-                                                                            crate::ast::BinOp::Sub,
-                                                                            Box::new(crate::ast::ExprTree::Number(2)))));
+    #[test]
+    fn test_bool(){
+        assert_eq!(parser::BoolCompParser::new().parse("true").unwrap(), Box::new(ExprTree::Bool(BoolType::True)));
+
+        assert_eq!(parser::BoolCompParser::new().parse("1 == 2 || 5 > 4 - 1").unwrap(), 
+                Box::new(ExprTree::LogNode(
+                        Box::new(ExprTree::LogNode(
+                            Box::new(ExprTree::Number(1)),
+                            LogOp::Eq,
+                            Box::new(ExprTree::Number(2))
+                        )),
+                        LogOp::Or,
+                        Box::new(ExprTree::LogNode(
+                            Box::new(ExprTree::Number(5)),
+                            LogOp::Gre,
+                            Box::new(ExprTree::BinNode(
+                                Box::new(ExprTree::Number(4)),
+                                BinOp::Sub,
+                                Box::new(ExprTree::Number(1))
+                            ))
+                        ))
+                )));
+
+        assert_eq!(parser::BoolCompParser::new().parse("true && false").unwrap(), 
+                Box::new(ExprTree::LogNode(
+                        Box::new(ExprTree::Bool(BoolType::True)),
+                        LogOp::And,
+                        Box::new(ExprTree::Bool(BoolType::False))
+                )));
+    }
+
+    #[test]
+    fn test_var(){
+        assert_eq!(parser::SeparateLinesParser::new().parse("let a: i32 = 12;").unwrap(), 
+                Box::new(ExprTree::AssignNode(
+                        Box::new(ExprTree::Var("a".to_string())),
+                        Type::I32,
+                        Box::new(ExprTree::Number(12)))
+            ));
+
+        assert_eq!(parser::SeparateLinesParser::new().parse("let b: bool = true;").unwrap(), 
+                Box::new(ExprTree::AssignNode(
+                        Box::new(ExprTree::Var("b".to_string())),
+                        Type::Bool,
+                        Box::new(ExprTree::Bool(BoolType::True)))
+            ));
+
+
+        assert_eq!(parser::SeparateLinesParser::new().parse("let b: bool = 1 >= 5;").unwrap(), 
+                Box::new(ExprTree::AssignNode(
+                        Box::new(ExprTree::Var("b".to_string())),
+                        Type::Bool,
+                        Box::new(ExprTree::LogNode(
+                            Box::new(ExprTree::Number(1)),
+                            LogOp::GrEq,
+                            Box::new(ExprTree::Number(5))
+                        )))
+            ));
+    }
+
+    #[test]
+    fn test_seq(){
+        assert_eq!(parser::SeparateLinesParser::new().parse("let a: i32 = 1; let b: bool = false;").unwrap(),
+            Box::new(ExprTree::SeqNode(
+                        Box::new(ExprTree::AssignNode(
+                            Box::new(ExprTree::Var("a".to_string())),
+                            Type::I32,
+                            Box::new(ExprTree::Number(1))
+                        )),
+                        Box::new(ExprTree::AssignNode(
+                            Box::new(ExprTree::Var("b".to_string())),
+                            Type::Bool,
+                            Box::new(ExprTree::Bool(BoolType::False))
+                        ))
+
+            ))
+        );
+    }
+
+    #[test]
+    fn test_if(){
+        assert_eq!(parser::SeparateLinesParser::new().parse("if(true){ let a: i32 = 2;};").unwrap(), 
+                Box::new(ExprTree::IfNode(
+                    Box::new(ExprTree::Bool(BoolType::True)),
+                    Box::new(ExprTree::AssignNode(
+                        Box::new(ExprTree::Var("a".to_string())),
+                        Type::I32,
+                        Box::new(ExprTree::Number(2))
+                    ))
+                )));
+
+        assert_eq!(parser::SeparateLinesParser::new().parse("if(true){ let a: i32 = 2;}else{let a: i32 = 2;};").unwrap(), 
+                Box::new(ExprTree::IfElseNode(
+                    Box::new(ExprTree::Bool(BoolType::True)),
+                    Box::new(ExprTree::AssignNode(
+                        Box::new(ExprTree::Var("a".to_string())),
+                        Type::I32,
+                        Box::new(ExprTree::Number(2))
+                    )),
+                    Box::new(ExprTree::AssignNode(
+                        Box::new(ExprTree::Var("a".to_string())),
+                        Type::I32,
+                        Box::new(ExprTree::Number(2))
+                    ))
+                )));
+
+        assert_eq!(parser::SeparateLinesParser::new().parse("if(true){ let a: i32 = 2;}else if(false){let a: i32 = 2;}else if(true){let a: i32 = 2;};").unwrap(), 
+                Box::new(ExprTree::IfElseNode(
+                    Box::new(ExprTree::Bool(BoolType::True)),
+                    Box::new(ExprTree::AssignNode(
+                        Box::new(ExprTree::Var("a".to_string())),
+                        Type::I32,
+                        Box::new(ExprTree::Number(2))
+                    )),
+                    Box::new(ExprTree::IfElseNode(
+                        Box::new(ExprTree::Bool(BoolType::False)),
+                        Box::new(ExprTree::AssignNode(
+                            Box::new(ExprTree::Var("a".to_string())),
+                            Type::I32,
+                            Box::new(ExprTree::Number(2))
+                        )),
+                        Box::new(ExprTree::IfNode(
+                            Box::new(ExprTree::Bool(BoolType::True)),
+                            Box::new(ExprTree::AssignNode(
+                                Box::new(ExprTree::Var("a".to_string())),
+                                Type::I32,
+                                Box::new(ExprTree::Number(2))
+                            ))
+                        ))
+                    ))
+                )));
+    }
 }
