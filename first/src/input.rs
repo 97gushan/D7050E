92bc7ee0f4a87983852521ee9c4592077286d449
97gushan <97gustavh@gmail.com>
2019-10-29 08:51:09 +0100

    updated input

--- first/src/input.rs
+++ first/src/input.rs
@@ -1,10 +1,18 @@
-
-
-fn main() -> i32 {
+fn foo() -> i32{
 
     let a: i32 = 1;
 
-  
+    while(a < 10){
+        a = a + 1;
+    }
 
-    return a;
+    if(a >= 10 || 9 == 20 - 5 && false){
+        return a;
+    }
+
+    return a + 5 * (2 - 5);
+}
+
+fn main() -> i32 {
+    return foo();
 }
